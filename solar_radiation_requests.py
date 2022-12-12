4816dfdf82a228f69ff0199f8583867ab7d47fbb
raminbeiranvand <beiranvand83@gmail.com>
2022-12-12 15:31:59 +0100

    final edit

--- solar_radiation_requests.py
+++ solar_radiation_requests.py
@@ -3,20 +3,15 @@ import pandas as pd
 
 url='https://power.larc.nasa.gov/api/temporal/daily/point'
 
-def historical_solar_radiation(lat, lon, start_date,end_date):
+def historical_solar_radiation(lat, lon):
     '''Return a day-by-day historical solar radiation for one year at a certain place, given its latitude and longitude.'''
     params= {
         'parameters':'ALLSKY_SFC_SW_DWN','community':'RE', 'latitude': lat,
-<<<<<<< HEAD
              'longitude': lon, 'start':'20210101','end':'20220101','format':'JSON'
-=======
-             'longitude': lon, 'start':start_date,'end':end_date,'format':'JSON'
->>>>>>> e89abecfe8bdb276f916efef6c8fd03fb5bc037b
     }
     solar_radiation = requests.get(url, params=params).json()['properties']['parameter']['ALLSKY_SFC_SW_DWN']
     df=pd.DataFrame([solar_radiation]).T.rename(columns={0:'radiation'})
     return df
 
 if __name__ == '__main__':
-    #historical_solar_radiation(lat, lon, start_date,end_date)
     print(historical_solar_radiation(51.5072, 0.1276, 20210101,20210331))
