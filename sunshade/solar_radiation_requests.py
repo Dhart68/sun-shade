1dcaff3c824e061aad245581b9d6b4c114f4b060
raminbeiranvand <beiranvand83@gmail.com>
2022-12-13 14:01:16 +0100

    changes still not done

--- sunshade/solar_radiation_requests.py
+++ sunshade/solar_radiation_requests.py
@@ -11,7 +11,12 @@ def historical_solar_radiation(lat, lon):
     }
     solar_radiation = requests.get(url, params=params).json()['properties']['parameter']['ALLSKY_SFC_SW_DWN']
     df=pd.DataFrame([solar_radiation]).T.rename(columns={0:'radiation'})
+
+    df.reset_index(inplace=True)
+    df['date'] = df['index'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
+
     return df
 
 if __name__ == '__main__':
-    print(historical_solar_radiation(51.5072, 0.1276, 20210101,20210331))
+    #historical_solar_radiation(lat, lon)
+    print(historical_solar_radiation(51.5072, 0.1276))
