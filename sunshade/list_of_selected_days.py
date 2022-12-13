f9173ba257c5e230eb1ec8ef7fc80ff3b67adff0
raminbeiranvand <66700649+raminbeiranvand@users.noreply.github.com>
2022-12-13 11:16:43 +0100

    Update list_of_selected_days.py

--- sunshade/list_of_selected_days.py
+++ sunshade/list_of_selected_days.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-def list_of_selected_days(start = '2021-01-03', end = '2021-12-26', n=2):
+def list_of_selected_days(start , end , n):
      # List of day of the year given a starting and endind day and a number of dates  
        
     day_list=pd.date_range(start, end,n) # 52 for all year, use 2, 4, 8 to test
     
     return day_list
+
+if __name__ == '__main__':
+    print(list_of_selected_days(start = '2021-01-03', end = '2021-12-26', n=2))
