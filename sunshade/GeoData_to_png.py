13c51ef28174838904dde1b19a42919cfb1931fc
David Hartnagel <hartnagel.david@gmail.com>
2022-12-15 14:26:45 +0100

    add POI on the graph

--- sunshade/GeoData_to_png.py
+++ sunshade/GeoData_to_png.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-def GeoData_to_png(path='./raw-data/GeoData', Image_Storage_directory='./raw-data/Images'):
+def GeoData_to_png(path='./raw-data/GeoData', Image_Storage_directory='./raw-data/Images', lat=40.70, lng =-74.01):
 
     dir_list = os.listdir(path)
 
@@ -37,7 +37,7 @@ def GeoData_to_png(path='./raw-data/GeoData', Image_Storage_directory='./raw-dat
         #buildings.plot(ax = ax,edgecolor='k',facecolor=(0,0,0,0))
         plt.sca(ax)
         plt.title(f'Sunshine time /{file_name.rsplit("_20", 1)[1]}')
-
+        plt.scatter(lng, lat, color = 'red')
         fname = f'{Image_Storage_directory}/{file_name[:-4]}.png'
         plt.savefig(fname, format ='png')
         fig.clear()
